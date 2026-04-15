857f032b0af56a4f15be833811f7a4c05a37a3d2
Bcv1018 <carrillobrandon98@gmail.com>
2026-04-14 20:48:53 -0700

    updated Ex1B_SQL

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,10 @@
 /*
 Sakila is a database used to collect inoformation about actors and films used by a movie rental store.
-/*
+*/
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+select * from film; -- Retrieved 1000 records
+
+
