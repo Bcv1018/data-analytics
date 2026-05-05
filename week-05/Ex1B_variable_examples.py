e603cc4b79b3d82553e7697096d980069382637d
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-05 15:07:09 -0700

    finished lab 1 of 1b

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,7 @@
+CustomerId = '12345' # I thought this variable would be to hold a customerID number but I am thinking it would better to put it as an integer?
+CustomerName = 'Buster Posey' # This variable was used to hold the customers name but it could be separted into two variables named first and last name
+CustomerGender = 'Male' # I thought this would be to hold the customers gender.
+CustomerDOB = '04-27-87' # I thought this would be the customers date of birth and maybe it should be a int? instead of a str or if there is a date syntax for data type it would be that? Maybe also make another variable for their age?
+DriversLicenseNum = 'f123456' # I thought this was customers driver license and maybe just include the digits instead of f or maybe not idk how DL are formatted and if they are different for each state.
+AutoPolicyNum = '56656' # This I really don't know but I am guessing its like a case number or a ticket number for an accident?
+# I am assuming that these varaibles are to hold car insurance customer's information
