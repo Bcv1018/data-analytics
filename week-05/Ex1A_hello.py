f3106b188e59a58fa85c93ac568629a8a3a340b9
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-05 14:53:23 -0700

    finished 1A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,22 @@
 print('Hello world')
+
+message = 'Hello world'
+print(message)
+
+#'Hello world' prints twice because it is written twice and python runs everything that is in your line of code.
+
+#Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+# One thing I noticed about the result is that it added 3 and .50 to make 3.5 it took out the zero in .50 and I am
+# assuming that it turned 3 into a float data type.
+
+cents = + .25
+print(cents)
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents' 
+print(d_str + ' ' + c_str) 
