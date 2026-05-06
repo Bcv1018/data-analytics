d7ca7b8d974c51cb9639e593cd26c8d5f8fd3065
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-05 23:11:18 -0700

    Finished area of rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,14 @@
+# Formula for area of a rectangle is A = wl so lenght * width
+
+# Width - Side A
+Width = 8
+# Length - Side B
+Length = 11
+
+# Formula for calculating area
+Area = Width * Length
+
+# Displaying the results
+print('Side A is: ', Width)
+print('Side B is: ', Length)
+print('The area of the rectangle is ', Area)
