93df40ac196c301bbb7f0e326e32b1354d3831a6
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-06 13:55:08 -0700

    finished tip amount calculation

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,15 @@
+# Tip amount calculation is total bill * tip percentage = tip amount
+
+# Total restaurant bill
+Bill = 78.35
+
+# Tip percentage for 15 percent
+Tip_percentage = .15
+
+# calculation for tip amount 
+Tip_Amount = Bill * Tip_percentage
+
+# Displaying the results
+print(f'The tip on a ${Bill:.2f} restaurant bill is ${Tip_Amount:.2f}')
+
+
