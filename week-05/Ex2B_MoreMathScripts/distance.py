eb66f2318f7c02414c0d9bdc28ac537a9e21d546
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-07 11:54:17 -0700

    updated comments

--- week-05/Ex2B_MoreMathScripts/distance.py
+++ week-05/Ex2B_MoreMathScripts/distance.py
@@ -1,8 +1,6 @@
-# # This script is for calculating distances between two  different coordinates
-# # Will be creating two different methods of finding the distance one using 
+# This script is for calculating distances between two  different coordinates
+# Distance formula using square roots: square root over (x2 - x1)^2 + (y2 - y1)^2 
 
-# # Distance formula using square roots: square root over (x2 - x1)^2 + (y2 - y1)^2 
-# # Distance formula using math.dist: math.distance(point1,point2)
 
 import math 
 
