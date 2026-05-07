4259fb7bbf209460e928758ca7d70e96407af68d
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-07 08:42:33 -0700

    Did federal taxes withheld calculation

---
+++ week-05/Ex2B_MoreMathScripts/taxes.py
@@ -0,0 +1,13 @@
+# formula for withholding federal taxes for salary
+
+# Amount of money per monthly Yearly made is 100k
+Monthly = 8333
+
+# Taxes
+Federal_taxes = .23
+
+# Calculation 
+
+Taxes_Withheld = Monthly * Federal_taxes
+
+print(f"The amount of taxes withheld monthly is {Taxes_Withheld:.2f}")
