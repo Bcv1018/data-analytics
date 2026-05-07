5029a063429139cb97f63ed5097ac19b68ff79c0
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-06 22:29:59 -0700

    Finished celsious to fahrenheit converter

---
+++ week-05/Ex2B_MoreMathScripts/c_to_f.py
@@ -0,0 +1,10 @@
+# Formula for celcius to fahrenheit is f = C * 1.8 + 32
+
+# Celsius 
+Celsius = int(input("Please type Celsius to be converted: "))
+
+# Converter calculation
+Convert = Celsius * 1.8 + 32
+
+# Displaying the results
+print(f'Celsius converted to Fahrenheit is: {Convert}')
