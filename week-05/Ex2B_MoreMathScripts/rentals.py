24c71f8a4600c1b2d1b53778a84930ac947210ad
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-10 13:13:32 -0700

    finished rentals calculation

--- week-05/Ex2B_MoreMathScripts/rentals.py
+++ week-05/Ex2B_MoreMathScripts/rentals.py
@@ -1,2 +1,28 @@
 # Formula for how many vans needed tourists/ van seats
-# Forumla for how many vans
+# Forumla for how much the vans cost: Van cost * needed vans
+# Formula for how much per person
+
+import math
+
+# Input variables to get values for certain things
+Tourists = int(input("How many tourists are there? "))
+Van_seats = int(input("How many seats in a van? " ))
+
+# How many vans needed
+Vans_needed = math.ceil(Tourists / Van_seats)
+
+print(f"The amount of vans needed is {Vans_needed}")
+# How much vans cost
+Vans_cost = 250 * Vans_needed
+
+print(f"The amount of cost for the vans is ${Vans_cost}")
+
+# How much it is per person
+Cost_per_person = Vans_cost / Tourists
+
+print(f"The cost per person to rent is ${Cost_per_person:.2f}")
+
+# A) My script said per person it will be $19.74
+# B) If I multiply that out it will be 750.12
+# C) For 3 vans it was $750
+# D) I have leftover money because in my script it formats the money to two decimal points and is not the exact number of the cost per person
