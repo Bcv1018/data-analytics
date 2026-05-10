d393914780a3c72317ea6de4e7503fbd91c9920e
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-10 14:43:50 -0700

    Started 2B movie_list

---
+++ week-05/Ex3B_SampleCollections/movie_list.py
@@ -0,0 +1,17 @@
+# Working with lists
+movie_list = ["Forrest Gump","The Batman", "Alien","Shrek","Scooby-Doo"]
+print(f"The list [movie_list] inlcudes {len(movie_list)} of my favorite movies")
+print(movie_list)
+
+# Using sorted()
+print(sorted(movie_list))
+print(movie_list)
+# The sorted() function lists the list in alphabetical order and without it it lists the list in the order the strings were put in
+
+# Using .sort()
+movie_list.sort()
+print(movie_list)
+
+#
+
+
