12c0b202ddc346835233a90ef9866af45a58de30
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-10 21:15:51 -0700

    changed code num name

--- week-05/Ex4B_MoreIfScripts/dept_converter.py
+++ week-05/Ex4B_MoreIfScripts/dept_converter.py
@@ -9,19 +9,19 @@
 # 20 Customer Relations
 
 # Input statement to get the use to type in the code 
-Code_num = int(input("Please type in department number: "))
+Dept_num = int(input("Please type in department number: "))
 
-if Code_num == 1:
+if Dept_num == 1:
     print("Marketing Department")
-elif Code_num == 5:
+elif Dept_num == 5:
     print("Human Resources Department")
-elif Code_num == 10:
+elif Dept_num == 10:
     print("Accounting Department")
-elif Code_num == 12:
+elif Dept_num == 12:
     print("Legal Department")
-elif Code_num == 18:
+elif Dept_num == 18:
     print("IT Department")
-elif Code_num == 20:
+elif Dept_num == 20:
     print("Customer Relations Department")
 else:
     print("Department number is not valid")
