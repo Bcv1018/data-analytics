0641ecdc69ad6cb8ae1e6b7d59558fcc50845e3e
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-10 22:56:41 -0700

    completed loop love

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,8 @@
+# creating a loop to say different things about loops
+
+adj = ["Amazing", "Awesome", "Excellent","Great","Fun"]
+
+for word in adj:
+    print(f"Loops are {word}!")
+
+print("I <3 loops")
