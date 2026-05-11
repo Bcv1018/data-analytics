73f6a1be9c9267fd735ab6c42fc9e65d2883d706
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-10 21:52:01 -0700

    completed greeting.py

--- week-05/Ex4B_MoreIfScripts/greeting.py
+++ week-05/Ex4B_MoreIfScripts/greeting.py
@@ -2,11 +2,15 @@
 
 current_hour = int(input("Please type in the current hour (0-23) "))
 
-if current_hour < 23 or current_hour > 4:
-        print("What are you doing up so late??")
-elif current_hour > 10:
+
+if current_hour < 10:
         print("Good Morning!")
-elif current_hour <= 10:
-        print("Good day!")
-elif current_hour > 17 :
+elif current_hour < 17:
+        print("Good Day!")
+else:
         print("Good Evening!")
+
+
+
+if current_hour >= 23 or current_hour < 4:
+    print("What are you doing up so late??")
