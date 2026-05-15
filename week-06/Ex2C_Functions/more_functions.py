d54c89888561e0d93e5bd7c22016d72077503654
Bcv1018 <carrillobrandon98@gmail.com>
2026-05-15 12:51:59 -0700

    Started 2C Lab 2

---
+++ week-06/Ex2C_Functions/more_functions.py
@@ -0,0 +1,7 @@
+def display_mailing_label(name,address,city,state,zip):
+    return f'{name}\n{address}\n{city},{state} {zip}'
+print(display_mailing_label('Brandon Carrillo-Valencia','123 Main St', 'San Jose','CA','12345'))
+
+def add_numbers(*):
+    
+    
